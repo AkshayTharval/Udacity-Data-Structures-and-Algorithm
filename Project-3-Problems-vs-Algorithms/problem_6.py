17f45d74f11aba4eb0194ca0ee036bfea000f0d1
Akshay Tharval <atharval@andrew.cmu.edu>
2020-03-08 12:42:35 -0400

    Changes made accoridng to the review (Added more examples)

--- Project-3-Problems-vs-Algorithms/problem_6.py
+++ Project-3-Problems-vs-Algorithms/problem_6.py
@@ -21,10 +21,17 @@ def get_min_max(ints):
     
     return (_min, _max)
 	
-## Example Test Case of Ten Integers
+print('=============================== Udacity Examples ===============================')
 import random
 
 l = [i for i in range(0, 10)]  # a list containing 0 - 9
 random.shuffle(l)
 
+print('=============================== Self Examples ===============================')
 print ("Pass" if ((0, 9) == get_min_max(l)) else "Fail")
+print ("Pass" if ((1, 9) == get_min_max([1,3,5,2,6,7,1,9])) else "Fail")
+print ("Pass" if ((12, 99) == get_min_max([12,15,26,33,56,79,99])) else "Fail")
+
+print('=============================== Edge Case ===============================')
+
+print ("Pass" if (None == get_min_max([])) else "Fail")
